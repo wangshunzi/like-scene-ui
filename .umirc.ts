55598fa2271d122bd9acdf604ccb45e65be7a2b3
chengfang <1601842304@qq.com>
2022-01-28 10:09:49 +0800

    update: 更新文档

--- .umirc.ts
+++ .umirc.ts
@@ -10,5 +10,22 @@ export default defineConfig({
   base: '/like-ui',
   publicPath: '/like-ui/',
   exportStatic: {},
+  navs: [
+    null, // null 值代表保留约定式生成的导航，只做增量配置
+    {
+      title: '设计',
+      path: '/',
+    },
+    {
+      title: '产品',
+      path: '/',
+      // 可通过如下形式嵌套二级导航菜单，目前暂不支持更多层级嵌套：
+      children: [
+        { title: '组件', path: '/' },
+        { title: '高阶组件', path: '/' },
+        { title: 'hooks', path: '/' },
+      ],
+    },
+  ],
   // more config: https://d.umijs.org/config
 });
